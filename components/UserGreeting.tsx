3234c0ad0859e7c550aaa7aa1aec156f3e71726a
Shomari Roberts <shomariroberts@gmail.com>
2023-07-22 20:14:52 -0400

    passing user instead of making another server call

--- components/UserGreeting.tsx
+++ components/UserGreeting.tsx
@@ -1,27 +1,13 @@
-import { getUserFromCookie } from '@/lib/auth';
-import { cookies } from 'next/headers';
+import { UserProps } from '@/lib/interfaces/interfaces';
 import { User } from 'react-feather';
-import LogoutButton from './LogoutButton';
 
-const getData = async () => {
-  const user = await getUserFromCookie(cookies() as any);
-
-  return user;
-};
-const UserGreeting = async () => {
-  const user = await getData();
-
+const UserGreeting = ({ user }: { user: UserProps }) => {
   return (
-    <div className='-mt-12 float-right'>
-      <div className='flex w-max justify-between items-center gap-4 font-medium text-slate-400'>
-        <div className='flex items-center gap-2'>
-          <User className='w-[1.5rem] h-[1.5rem] rounded-full border-2 border-blue-300 bg-blue-50 text-blue-300' />{' '}
-          <span className='hidden sm:block text-base'>
-            {user?.firstName} {user?.lastName}
-          </span>
-        </div>
-        <LogoutButton />
-      </div>
+    <div className='flex items-center gap-2'>
+      <User className='w-[1.5rem] h-[1.5rem] rounded-full border-2 border-blue-300 bg-blue-50 text-blue-300' />{' '}
+      <span className='hidden sm:block text-base'>
+        {user?.firstName} {user?.lastName}
+      </span>
     </div>
   );
 };
