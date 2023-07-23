06b9256b0e6846da72bdf4b777de2a343159fbbb
Shomari Roberts <shomariroberts@gmail.com>
2023-07-22 20:36:29 -0400

    wired up AuthContext

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/global.css';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import { AuthProvider } from '../contexts/AuthContext';
 
 export default function IndexLayout({
   children,
@@ -8,14 +9,16 @@ export default function IndexLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <head />
-      <body className='flex flex-col h-screen md:max-w-4xl lg:max-w-6xl mx-auto px-8'>
-        <Header />
-        <main>{children}</main>
-        <div id='modal' className='mx-0 my-0'></div>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+        <head />
+        <body className='flex flex-col h-screen md:max-w-4xl lg:max-w-6xl mx-auto px-8'>
+          <Header />
+          <main>{children}</main>
+          <div id='modal' className='mx-0 my-0'></div>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
