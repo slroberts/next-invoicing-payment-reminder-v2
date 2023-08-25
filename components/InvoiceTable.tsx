6a260d181aaea081b288e46510392ecbf0a8b686
Shomari Roberts <shomariroberts@gmail.com>
2023-08-25 00:15:11 -0400

    added payment status and updated layout

--- components/InvoiceTable.tsx
+++ components/InvoiceTable.tsx
@@ -10,16 +10,19 @@ const InvoiceTable: FC<InvoiceProps> = ({ invoices = [] }) => {
 
   return (
     <div className='mt-4 w-full min-w-full divide-y divide-slate-200 bg-slate-100 border-collapse border'>
-      <div className='hidden sm:flex'>
+      <div className='hidden md:flex'>
         <div className='grid grid-flow-col gap-4 justify-stretch w-full'>
           <div className='px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>
-            INVOICE ID
+            Invoice Id
+          </div>
+          <div className='px-0 md:px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>
+            Due Date
           </div>
           <div className='px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>
-            DUE DATE
+            Email Status
           </div>
           <div className='px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>
-            STATUS
+            Payment Status
           </div>
         </div>
         <div className='px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>
@@ -30,33 +33,39 @@ const InvoiceTable: FC<InvoiceProps> = ({ invoices = [] }) => {
         {invoices.map((invoice) => (
           <div
             key={invoice.id}
-            className='flex py-4 sm:py-2 text-sm font-semibold transition-colors hover:bg-blue-50'
+            className='flex py-4 md:py-2 text-sm font-semibold transition-colors hover:bg-blue-50'
           >
             <Link
               key={invoice.id}
               href={`/dashboard/client/invoice/${invoice.id}`}
-              className='grid grid-cols-2 sm:grid-cols-3  sm:grid-flow-col sm:gap-4 justify-stretch w-full'
+              className='grid grid-cols-2 md:grid-cols-4 md:grid-flow-col md:gap-4 justify-stretch w-full'
             >
-              <div className='px-6 py-2 font-semibold w-32'>
-                <span className='block sm:hidden text-left text-xs font-medium text-slate-500 uppercase tracking-wider mb-1'>
-                  INVOICE ID{' '}
+              <div className='px-6 py-2 font-semibold'>
+                <span className='block md:hidden text-left text-xs font-medium text-slate-500 uppercase tracking-wider mb-1'>
+                  Invoice Id{' '}
                 </span>
                 {invoice.id.slice(0, 8)}
               </div>
-              <div className='px-6 py-2 w-40 sm:ml-1'>
-                <span className='block sm:hidden text-left text-xs font-medium text-slate-500 uppercase tracking-wider mb-1'>
-                  DUE DATE{' '}
+              <div className='px-6 md:px-0 py-2 md:ml-1'>
+                <span className='block md:hidden text-left text-xs font-medium text-slate-500 uppercase tracking-wider mb-1'>
+                  Due Date{' '}
                 </span>
                 {invoice.due}
               </div>
-              <div className='px-6 py-2 w-32 sm:ml-1'>
-                <span className='block sm:hidden text-left text-xs font-medium text-slate-500 uppercase tracking-wider mb-1'>
-                  STATUS{' '}
+              <div className='px-6 md:px-0 py-2 text-left md:-ml-4'>
+                <span className='block md:hidden text-left text-xs font-medium text-slate-500 uppercase tracking-wider mb-1'>
+                  Email Status{' '}
                 </span>
                 {invoice.status === 'SENT' ? 'Sent' : 'Not Sent'}
               </div>
+              <div className='px-6 md:px-0 py-2 md:-ml-4'>
+                <span className='block md:hidden text-left text-xs font-medium text-slate-500 uppercase tracking-wider mb-1'>
+                  Payment Status{' '}
+                </span>
+                {invoice.paymentStatus === 'PAID' ? 'Paid' : 'Not Paid'}
+              </div>
             </Link>
-            <div className='px-6 py-2 sm:mr-4'>
+            <div className='px-6 py-2 md:mr-4'>
               <DeleteButton invoice={invoice} />
             </div>
           </div>
