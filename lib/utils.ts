2ecda9df4661f70683481350313994889e4ef2e5
Shomari Roberts <shomariroberts@gmail.com>
2023-08-25 00:09:30 -0400

    made calculateSubTotal reusable

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,17 @@
-export default function capitalizeWords(str: string) {
+import { ItemProps } from './interfaces/interfaces';
+
+export function capitalizeWords(str: string) {
   return str.replace(/(^|\s)\S/g, function (firstLetter) {
     return firstLetter.toUpperCase();
   });
+}
+
+export function calculateSubTotal(items: ItemProps[]) {
+  let sum = 0;
+
+  for (let item of items) {
+    sum += item.price! * item.hours!;
+  }
+
+  return parseFloat(sum.toFixed(2));
 }
