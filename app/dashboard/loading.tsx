fcc8b4da59392539bc75a11e0c5c3d0229028cf6
Shomari Roberts <shomariroberts@gmail.com>
2023-08-24 22:28:19 -0400

    made LoadingSpinner its own component

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,7 +1,5 @@
+import LoadingSpinner from '@/components/LoadingSpinner';
+
 export default function DashboardPageLoader() {
-  return (
-    <div className='flex justify-center items-center w-full h-full'>
-      <div className='mt-80 w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-blue-400'></div>
-    </div>
-  );
+  return <LoadingSpinner />;
 }
---
+++ components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function LoadingSpinner() {
+  return (
+    <div className='flex justify-center items-center w-full h-full'>
+      <div className='mt-80 w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-blue-400'></div>
+    </div>
+  );
+}
