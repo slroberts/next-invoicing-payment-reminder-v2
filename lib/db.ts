34dac14364792e3de66bcd02f8e5a814f911274c
Shomari Roberts <shomariroberts@gmail.com>
2023-11-14 11:32:50 -0500

    refactored prisma setup

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,14 @@
 import { PrismaClient } from '@prisma/client';
-
 declare global {
-  // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
 
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
+const prismaGlobal = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') {
+  if (!globalThis.prisma) {
+    globalThis.prisma = prismaGlobal;
   }
-  prisma = global.cachedPrisma;
 }
 
-export const db = prisma;
+export const db = prismaGlobal;
