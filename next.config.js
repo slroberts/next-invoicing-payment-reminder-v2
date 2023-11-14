de05cda7d9ce465bc562a290fe1000b7e6a13ad4
Shomari Roberts <shomariroberts@gmail.com>
2023-11-14 11:04:44 -0500

    no longer need experimental since stable

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ['bcrypt'],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
