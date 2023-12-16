91ec5c66ecaeebffc8e4245d2f63bcb91e6ca041
Shomari Roberts <shomariroberts@gmail.com>
2023-12-15 23:10:29 -0500

    separeated and improved logic

--- pages/api/sendInvoice.ts
+++ pages/api/sendInvoice.ts
@@ -1,13 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Resend } from 'resend';
 import { db } from '@/lib/db';
-import { InvoiceEmailProps } from '@/lib/interfaces/interfaces';
+import {
+  IClient,
+  IInvoice,
+  InvoiceEmailProps,
+} from '@/lib/interfaces/interfaces';
 import { InvoiceEmail } from '@/components/templates/invoice-email';
+import { CreateEmailOptions } from 'resend/build/src/emails/interfaces';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const FROM_EMAIL =
   'Invoicing Payment Reminder <noreply@invoicingpaymentreminder.com>';
+
+// Check if the data is valid
+function isValidData(client: IClient, invoice: IInvoice) {
+  return !(!client || !invoice);
+}
+
+// Check if it's time to send the email
+function shouldSendEmail(
+  today: Date,
+  reminderDate: Date,
+  invoiceDueDate: Date,
+  paymentStatus: string,
+  status: string
+) {
+  return (
+    ((today.getTime() === reminderDate.getTime() ||
+      today.getTime() === invoiceDueDate.getTime()) &&
+      paymentStatus === 'NOT_PAID') ||
+    status === 'NOT_SENT'
+  );
+}
+
+// Send the email
+async function sendEmail(emailConfig: CreateEmailOptions, invoiceId: string) {
+  const { data, error } = await resend.emails.send(emailConfig);
+
+  if (error) {
+    console.error(`Error sending email for invoice ${invoiceId}:`, error);
+    throw new Error('Failed to send email');
+  }
+
+  return data;
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,7 +57,7 @@ export default async function handler(
     const { invoice, items } = emailContent;
 
     // Check for invalid data
-    if (!client || !invoice) {
+    if (!isValidData(client, invoice)) {
       return res.status(400).json({ message: 'Invalid data provided' });
     }
 
@@ -27,14 +65,18 @@ export default async function handler(
     today.setHours(0, 0, 0, 0);
     const invoiceDueDate = new Date(invoice.due);
     const reminderDate = new Date(invoiceDueDate);
-    reminderDate.setDate(invoiceDueDate.getDate() - 10);
+    reminderDate.setDate(invoiceDueDate.getDate() - 3);
 
-    const shouldSendEmail =
-      (today.getTime() === reminderDate.getTime() ||
-        today.getTime() === invoiceDueDate.getTime()) &&
-      invoice.paymentStatus === 'NOT_PAID';
+    const shouldSend = shouldSendEmail(
+      today,
+      reminderDate,
+      invoiceDueDate,
+      invoice.paymentStatus,
+      invoice.status
+    );
 
-    if (shouldSendEmail || invoice.status === 'NOT_SENT') {
+    // If conditions are met, send the email
+    if (shouldSend) {
       const invoiceEmailProps: InvoiceEmailProps = {
         userId: user.id,
         userName: `${user.firstName} ${user.lastName}`,
@@ -54,7 +96,6 @@ export default async function handler(
         stripeAccountId: user.stripeAccountId,
       };
 
-      // Email configuration
       const emailConfig = {
         from: FROM_EMAIL,
         to: client.email,
@@ -64,12 +105,7 @@ export default async function handler(
       };
 
       // Send email
-      const { data, error } = await resend.emails.send(emailConfig);
-
-      if (error) {
-        console.error('Error sending email:', error);
-        return res.status(400).json({ error: error.message });
-      }
+      const data = await sendEmail(emailConfig, invoice.id);
 
       // Update invoice status in the database
       await db.invoice.update({
@@ -78,7 +114,7 @@ export default async function handler(
       });
 
       console.log('Email sent successfully:', data);
-      return res.status(200).json({ data });
+      return res.status(200).json({ message: 'Email sent successfully' });
     }
   } catch (error) {
     console.error('Error in handler:', error);
